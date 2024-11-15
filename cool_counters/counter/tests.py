3cd2ce511e42dc82d65eaf8b6b4c48a57c674c8c
tinhtq <truongquangtinh1997@gmail.com>
2024-11-15 17:03:07 +0700

    update github action

--- cool_counters/counter/tests.py
+++ cool_counters/counter/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Counter
 
-# Create your tests here.
+class CounterViewTests(TestCase):
+    def test_counter_creation(self):
+        response = self.client.get(reverse('counter:index'))  # Replace 'counter:index' with the correct namespace if needed.
+        self.assertEqual(response.status_code, 200)
+        counter = Counter.objects.get(key='counter')
+        self.assertEqual(counter.value, 1)
+
